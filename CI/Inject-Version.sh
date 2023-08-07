3568b2f93f9d766785afb958da5dfd3560fba7ec
RikedyP <rikedyp@gmail.com>
2023-08-07 14:27:22 +0100

    get version number from last tag in git

--- CI/Inject-Version.sh
+++ CI/Inject-Version.sh
@@ -4,8 +4,8 @@ raw_version=`cat APLSource/version.apla | grep "'[0-9]\+\.[0-9]\+\.0-\?\w\+\?" |
 major_minor=`echo ${raw_version} | grep -o "[0-9]\+\.[0-9]\+"`
 special=`echo ${raw_version} | grep -o "\-\w\+$"`
 
-last_release=`curl https://api.github.com/repos/Dyalog/APLAutoHotKey/releases?per_page=1`
-last_patch=`echo $last_release | sed -r 's/.*tag_name": "v[0-9]+.[0-9]+.([0-9]+).*/\1/p' | head -1`
+last_release=`git describe --tags --abbrev=0`
+last_patch=`echo $last_release | sed -r 's/v[0-9]+.[0-9]+.([0-9]+).*/\1/p' | head -1`
 patch=$(($last_patch + 1))
 
 full_version=${major_minor}.${patch}${special}
