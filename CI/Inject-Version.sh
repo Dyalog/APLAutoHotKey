e75a2b05445ea6f1e98de1e746ca4f5ac219488b
RikedyP <rikedyp@gmail.com>
2023-08-15 13:21:02 +0100

    patch number for new release OR building release artifacts from current tag

--- CI/Inject-Version.sh
+++ CI/Inject-Version.sh
@@ -4,9 +4,11 @@ raw_version=`cat APLSource/version.apla | grep "'[0-9]\+\.[0-9]\+\.0-\?\w\+\?" |
 major_minor=`echo ${raw_version} | grep -o "[0-9]\+\.[0-9]\+"`
 special=`echo ${raw_version} | grep -o "\-\w\+$"`
 
-last_release=`git describe --tags --abbrev=0`
-last_patch=`echo $last_release | sed -r 's/v[0-9]+.[0-9]+.([0-9]+).*/\1/p' | head -1`
-patch=$(($last_patch + 1))
+# We have checked out some specific tag (a release we need to build) or the HEAD of the trunk (about to publish a new release)
+# Get the last tag before this commit (HEAD^). Our patch number (this tagged release or the new release we are about to publish) is previous_patch+1
+previous_release=`git describe --tags --abbrev=0 HEAD^`
+previous_patch=`echo $last_release | sed -r 's/v[0-9]+.[0-9]+.([0-9]+).*/\1/p' | head -1`
+patch=$(($previous_patch + 1))
 
 full_version=${major_minor}.${patch}${special}
 echo ${full_version}
